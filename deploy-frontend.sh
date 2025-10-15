054003cada817fe299558ccb3a8e4c3d6dfbcb1e
Shayan Halder <shayanhalder2389@gmail.com>
2025-10-15 14:17:06 -0700

    add frontend deployment script

---
+++ deploy-frontend.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+directory="$HOME/hayeslab-BLANT/frontend"
+cd $directory
+echo "Building static production files..."
+npm run build
+echo "Copying build files into /var/www/BLANT_Website/..."
+sudo cp -r dist/* /var/www/BLANT_Website/
+echo "Adding read permissions..."
+sudo chmod -R 755 /var/www/BLANT_Website
+sudo ln -s /etc/nginx/sites-available/hayeslab /etc/nginx/sites-enabled/
+sudo nginx -t
+echo "Restarting nginx server..."
+sudo systemctl reload nginx
+sudo systemctl restart nginx
+echo "Nginx status:"
+sudo systemctl status nginx
