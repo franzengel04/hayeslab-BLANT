440d5fdff6235f114f886fdd2766df2cb9f8f525
Shayan Halder <shayanhalder2389@gmail.com>
2025-10-15 14:19:03 -0700

    add backend deployment script

---
+++ deploy-backend.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+directory="$HOME/hayeslab-BLANT/backend"
+cd $directory
+if tmux has-session -t blant-backend 2>/dev/null; then # restart backend if tmux session already exists
+    tmux send-keys -t blant-backend C-c
+    tmux send-keys -t blant-backend "npm run dev" C-m
+else # otherwise create tmux session and start fresh backend instance
+    tmux new-session -d -s blant-backend
+    tmux send-keys -t blant-backend "npm run dev" C-m
+    tmux detach-client
+fi
