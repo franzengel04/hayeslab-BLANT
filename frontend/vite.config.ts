a4d7dd8f1f43a25fe55ff4eeb7c37a62b0de70a5
Shayan Halder <shayanhalder2389@gmail.com>
2025-10-15 14:47:50 -0700

    add viteconfig for blant routes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/blant/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    origin: 'http://hayeslab.ics.uci.edu',
+    hmr: {
+      protocol: 'ws',
+      host: 'hayeslab.ics.uci.edu',
+      port: 5173
+    }
+  }
 })
