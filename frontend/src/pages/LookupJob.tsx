25e94bfe4bf76d8004109a5ccfb79744ea5f66bd
Shayan Halder <shayanhalder2389@gmail.com>
2025-10-20 13:16:10 -0700

    make job output page full screen width

--- frontend/src/pages/LookupJob.tsx
+++ frontend/src/pages/LookupJob.tsx
@@ -9,6 +9,7 @@ import LoadingCircle from '../components/LoadingCircle';
 const LookupJob: React.FC = () => {
   const { id } = useParams<{ id?: string }>();
   const [jobId, setJobId] = useState('');
+  const [searchJobId, setSearchJobId] = useState('');
   const [jobOutput, setJobOutput] = useState<string | null>(null);
   const navigate = useNavigate();
 
@@ -27,61 +28,79 @@ const LookupJob: React.FC = () => {
   }
 
   useEffect(() => {
+    console.log('useEffect id:', id);
     if (id) {
       setJobId(id);
       getJobStatus(id);
-    }
+    } else {
+      setJobId('');
+      setJobOutput(null);
+      setSearchJobId('');
+    } 
   }, [id]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    navigate(`/lookup-job/${jobId}`)
+    setJobId(() => searchJobId);
+    navigate(`/lookup-job/${searchJobId}`)
+  };
+
+  const handleBack = () => {
+    setSearchJobId('');
+    navigate('/lookup-job');
   };
 
 
   return (
     <div className="lj-pageContainer">
-      <div className="lj-lookupBox">
-        <h2 className="lj-title">Job Lookup</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="lj-formGroup">
-            <label htmlFor="jobId" className="lj-label">Job ID To Search For:</label>
-            <input
-              type="text"
-              id="jobId"
-              className="lj-input"
-              placeholder="Previous Job ID"
-              value={jobId}
-              onChange={(e) => setJobId(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="lj-submitButton">
+      {
+        jobId === '' && (
+          <div className="lj-lookupBox">
+            <h2 className="lj-title">Job Lookup</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="lj-formGroup">
+                <label htmlFor="jobId" className="lj-label">Job ID To Search For:</label>
+                <input
+                  type="text"
+                  id="jobId"
+                  className="lj-input"
+                  placeholder="Previous Job ID"
+                  value={searchJobId}
+                  onChange={(e) => setSearchJobId(e.target.value)}
+                />
+              </div>
+              <button type="submit" className="lj-submitButton">
+                {
+                  (jobOutput === null && id != undefined) ? (
+                    <LoadingCircle />
+                  ) : (
+                    'Submit'
+                  )
+                }
+              </button>
+            </form>
             {
-              (jobOutput === null && id != undefined) ? (
-                <LoadingCircle />
-              ) : (
-                'Submit'
+              jobOutput === null && id != undefined && (
+                <div className="lj-output">
+                  <h3 className="lj-outputTitle">Job Output</h3>
+                  Job is still being processed...
+                </div>
               )
             }
-          </button>
-        </form>
-        {
+          </div>
+        )
+      }
+      
+      {
           jobOutput && (
             <div className="lj-output">
+              <button onClick={handleBack} className="lj-backButton"> Back </button>
               <h3 className="lj-outputTitle">Job Output</h3>
+              <p> Job ID: {jobId} </p> 
               <pre className="lj-outputContent">{jobOutput}</pre>
             </div>
           )
-        }
-        {
-          jobOutput === null && id != undefined && (
-            <div className="lj-output">
-              <h3 className="lj-outputTitle">Job Output</h3>
-              Job is still being processed...
-            </div>
-            )
-        }
-      </div>
+      }
     </div>
   );
 };
