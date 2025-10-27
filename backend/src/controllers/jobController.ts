90ad116b0b3bbe4dfcb531c391e57d2a0d4cc810
Shayan Halder <shayanhalder2389@gmail.com>
2025-10-27 13:50:45 -0700

    fix jobdir again

--- backend/src/controllers/jobController.ts
+++ backend/src/controllers/jobController.ts
@@ -199,7 +199,7 @@ const getJobStatus = async (req: GetJobResultsRequest, res: Response, next: Next
 
 
         // const jobDir = path.resolve(path.join(__dirname, '../../process', jobId));
-        const jobDir = path.resolve("../../../process", jobId);
+        const jobDir = path.resolve("../process", jobId);
 
         // // Check if job directory exists
         // if (!fs.existsSync(jobDir) || !fs.lstatSync(jobDir).isDirectory()) {
