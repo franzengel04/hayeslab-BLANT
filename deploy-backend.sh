8c0e91862a8548132c995e54f961811bf1541ea9
Shayan Halder <shayanhalder2389@gmail.com>
2025-11-06 15:04:04 -0800

    fix typo

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -6,20 +6,20 @@ npm run build
 sudo systemctl enable --now redis-server
 tmux_session_name=$(basename "$(pwd)")
 
-if tmux has-session -t $tmux_session_named 2>/dev/null; then # restart backend if tmux session already exists
-    tmux send-keys -t $tmux_session_named C-c
-    tmux send-keys -t $tmux_session_named "node ./dist/src/index.js" C-m
+if tmux has-session -t $tmux_session_name 2>/dev/null; then # restart backend if tmux session already exists
+    tmux send-keys -t $tmux_session_name C-c
+    tmux send-keys -t $tmux_session_name "node ./dist/src/index.js" C-m
 else # otherwise create tmux session and start fresh backend instance
-    tmux new-session -d -s $tmux_session_named
-    tmux send-keys -t $tmux_session_named "node ./dist/src/index.js" C-m
+    tmux new-session -d -s $tmux_session_name
+    tmux send-keys -t $tmux_session_name "node ./dist/src/index.js" C-m
     tmux detach-client
 fi
 
-if tmux has-session -t $tmux_session_named-worker; then
-    tmux send-keys -t $tmux_session_named-worker C-c
-    tmux send-keys -t $tmux_session_named-worker "node ./dist/src/services/jobWorkers.js" C-m
+if tmux has-session -t $tmux_session_name-worker; then
+    tmux send-keys -t $tmux_session_name-worker C-c
+    tmux send-keys -t $tmux_session_name-worker "node ./dist/src/services/jobWorkers.js" C-m
 else
-    tmux new-session -d -s $tmux_session_named-worker
-    tmux send-keys -t $tmux_session_named-worker "node ./dist/src/services/jobWorkers.js" C-m
+    tmux new-session -d -s $tmux_session_name-worker
+    tmux send-keys -t $tmux_session_name-worker "node ./dist/src/services/jobWorkers.js" C-m
     tmux detach-client
 fi
